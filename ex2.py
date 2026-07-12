377f3556828171ee3b3de44cada1d4a7a952d7e9
denisrtorres <denisrtorres18@hotmail.com>
2026-07-12 12:00:41 -0300

    commit 2.2

--- ex2.py
+++ ex2.py
@@ -19,3 +19,26 @@ while True:
 
         tentativas += 1
         historico.append(palpite)
+        if palpite == numero:
+            print(f"\nParabéns! Você acertou em {tentativas} tentativa(s).")
+            break
+
+        elif palpite > numero:
+            print("Muito alto!")
+
+        else:
+            print("Muito baixo!")
+
+        print(f"Tentativas restantes: {7 - tentativas}")
+
+    else:
+        print("\nVocê perdeu!")
+        print(f"O número era {numero}.")
+
+    print("Histórico de tentativas:", historico)
+
+    jogar = input("\nDeseja jogar novamente? (s/n): ").lower()
+
+    if jogar != "s":
+        print("Obrigado por jogar!")
+        break
